7bcf194309310430d2b7201d75e4335807c08815
Autumn Farris <autumn@autumnfarris.com>
2025-08-26 23:49:38 -0400

    support development environment routing

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
   trailingSlash: true, // important for GitHub Pages routing
-  basePath: "/personal-website",
-  assetPrefix: "/personal-website"
+  // Only apply basePath and assetPrefix for production builds
+  ...(isDev ? {} : {
+    basePath: "/personal-website",
+    assetPrefix: "/personal-website"
+  })
 };
 
 export default nextConfig;
